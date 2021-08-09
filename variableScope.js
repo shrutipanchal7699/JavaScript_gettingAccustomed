706564cdb2b6d7c535a6627ab404cdebf04cc577
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-08-09 12:04:21 -0700

    variableScope

---
+++ variableScope.js
@@ -0,0 +1,20 @@
+var myNum = 32;
+var myResult = "Success";
+
+
+function randomizer(limit) {
+    var randomNumber = Math.floor(Math.random() * limit);
+
+    var myNum = randomNumber;
+
+    console.log("myNum is", myNum);
+    console.log("Global myNum is", this.myNum);
+
+    console.log("Out result is", myResult);
+    return myNum;
+}
+
+console.log('\n')
+randomizer(10);
+console.log(myNum);
+
