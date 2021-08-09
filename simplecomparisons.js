a18cf5efa6ce1db7b276b159cb83312d7806faaf
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-08-08 21:46:14 -0700

    adding simplecomaprisons file

---
+++ simplecomparisons.js
@@ -0,0 +1,21 @@
+let one = 1, two = 2;
+one === one
+console.log(one == '1');
+console.log(one == two);
+console.log(++one);
+console.log(one);
+console.log(one++);
+console.log(one + "\n");
+
+
+// for (var i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+let num = [1, 2, 3, 4, 5]
+for (var i in num) {
+    console.log(i);
+}
+
+
+
+
